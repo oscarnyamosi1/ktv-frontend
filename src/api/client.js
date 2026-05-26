020af43046a5821499c20ee5771b5a968acba786
malik taylor l-mint <oscarnyamosi2@gmail.com>
2026-05-26 21:50:22 +0300

    renale login with slash to no slash

--- src/api/client.js
+++ src/api/client.js
@@ -62,7 +62,7 @@ api.interceptors.response.use(
         processQueue(refreshError)
 
         if (window.location.pathname !== '/login/') {
-          window.location.href = '/login/'
+          window.location.href = '/login'
         }
 
         return Promise.reject(refreshError)
